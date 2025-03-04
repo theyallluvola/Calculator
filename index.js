a0c22fbc5e3ae7194d399f0d0950d22cbf1ada35
olaoluwaigun <igunolly@gmail.com>
2025-03-04 08:22:59 -0500

    Done

---
+++ index.js
@@ -0,0 +1,152 @@
+const inputBox = document.getElementById("input");
+const expressionDiv = document.getElementById("expression");
+const resultDiv = document.getElementById("result");
+
+let expression = "";
+let result = "";
+
+function buttonClick(event) {
+    const target = event.target;
+    const action = target.dataset.action;
+    const value = target.dataset.value;
+    
+    switch (action) {
+        case "number":
+            addValue(value);
+            break;
+        case "clear":
+            clear();
+            break;
+        case "backspace":
+            backspace();
+            break;
+        case "addition":
+        case "subtraction":
+        case "multiplication":
+        case "division":
+            if (expression === '' && resut !== '') {
+                startFromResult(value);
+            } else if (expression !== '' && isLastCharOperator()) {
+                addValue(value);
+            }
+            break;
+        case "submit":
+            SubmitEvent()
+            break;
+        case "negate":
+            negate()
+            break;
+        case "mod":
+            percentage()
+            break;
+        case "decimal":
+            decimal(value)
+            break;
+            
+    }
+    
+    updateDisplay(expression, result);
+}
+
+inputBox.addEventListener('click', buttonClick);
+
+function addValue(value) {
+    if (value === '.') {
+        // Find the last index of the last operator in the expression
+        const lastOperatorIndex = expression.search(/[+\-*/]/);
+        // Find the index of the last decimal in the expression 
+        const lastDecimalIndex = expression.lastIndexOf('.');
+        // Find the index of the last number in the expression
+        const lastNumberIndex = Math.max(
+            expression.lastIndexOf('+'),
+            expression.lastIndexOf('-'),
+            expression.lastIndexOf('*'),
+            expression.lastIndexOf('/'),
+        );
+        // Check if this is the first decimal in the current number or if the expression is empty
+        if (
+            (lastDecimalIndex < lastOperatorIndex || 
+            lastDecimalIndex < lastNumberIndex || 
+            lastDecimalIndex === -1) && 
+            (expression === '' || expression.slice
+            (lastNumberIndex + 1).indexOf('.') === -1) 
+        )   {
+            expression += value;
+            }
+    } else {
+        expression += value;
+    }
+}
+
+function updateDisplay(expression, result) {
+    expressionDiv.innerHTML = expression;
+    resultDiv.innerHTML = result;
+}
+
+function clear() {
+    expression = "";
+    result = "";
+}
+
+function backspace() {
+    expression = expression.slice(0, -1);
+    console.log(expression);
+}
+
+function isLastCharOperator() {
+    return isNaN(parseInt(expression[expression.slice(- 1)]));
+}
+
+function startFromResult(value) {
+    expression = result + value;
+    result = "";
+}
+
+function submit() {
+    result = evaluateExpression();
+    expression = "";
+}
+
+function evaluateExpression() {
+    const evalResult = eval(expression);
+    return isNaN(evalResult) || !isFinite(evalResult) 
+    ? "Error" 
+    : evalResult < 1 
+    ? parseFloat(evalResult.toFixed(10)) 
+    : parseFloat(evalResult.toFixed(2));
+}
+
+function negate() {
+    //Negate the result if expression is not empty and result is present
+    if (expression !== '' && result !== '') {
+        result = -result;
+        // Toggle the sign of the expression if its not already negative and its not empty
+    } else if (!expression.startsWith('-') && expression !== '') {
+        expression = '-' + expression;
+        // Remove the negative sign from the expression if its already negative.
+    } else if (expression.startsWith('-')) {
+        expression = expression.slice(1);
+    }
+}
+
+function percentage() {
+    // Evaluate the expression, else it will take the percentage of only the first number
+    if (expression !== '') {
+        result = evaluateExpression();
+        expression = '';
+        if (!isNaN(result) && isFinite(result)) {
+            result /= 100;
+        } else {
+            result = 'Error';
+        }
+    } else if (result !== '') {
+        // If the expression is empty but the result exists, divide by 100
+        result = parseFloat(result) / 100;
+    }
+}
+
+function decimal(value) {
+    if (!expression.endsWith('.') && !isNaN(expression.slice(-1))) {
+        addValue('value');
+    }
+}
